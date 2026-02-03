229880b04c5947ed675ce1ce048861ca1d418606
jlk31 <164064098+jlk31@users.noreply.github.com>
2026-02-03 17:39:11 +0000

    Cut and pasted code from yesterday's work.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,73 @@
+from microbit import *
 
+#====================================
+# Life support modules
+#====================================
+
+# O2 level
+
+oxy_level_input = input.light_level()
+
+# CO2 level
+
+
+
+# Pressure level
+
+#====================================
+# Power system modules
+#====================================
+
+# Battery percentage
+
+def battery_level():
+
+    battery = 100
+
+    if battery < 25:
+        basic.show_string("Warning")
+
+    elif battery < 10: 
+        basic.show_string("Critical")
+
+    else:
+        basic.show_number(battery)
+
+    basic.pause(5000)
+
+    
+
+    
+
+
+
+# Solar input level
+
+
+
+# Consumption level
+
+
+
+
+#====================================
+# Navigation modules
+#====================================
+
+# Heading
+
+
+
+# Speed
+
+
+
+# Altitude
+
+
+
+# on_forever method
+
+def on_forever():
+    pass
+basic.forever(on_forever)
